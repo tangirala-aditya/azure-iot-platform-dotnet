39c17bd71feb6fb8df492f06e0052574f9f0ca0b
AARM6ZZ <aarm6zz@mmm.com>
2020-08-18 21:04:26 +0530

    Changed order of preprocessor

--- src/services/iothub-manager/WebService/Startup.cs
+++ src/services/iothub-manager/WebService/Startup.cs
@@ -72,13 +72,12 @@ namespace Mmm.Iot.IoTHubManager.WebService
         private static void SetupTelemetry(IApplicationBuilder app, AppConfig config)
         {
             var configuration = app.ApplicationServices.GetService<TelemetryConfiguration>();
-            configuration.TelemetryProcessorChainBuilder.Use(next => new HealthProbeTelemetryProcessor(next));
-            configuration.TelemetryProcessorChainBuilder.Build();
             var builder = configuration.DefaultTelemetrySink.TelemetryProcessorChainBuilder;
 
             // Using fixed rate sampling
             double fixedSamplingPercentage = config.Global.FixedSamplingPercentage == 0 ? 10 : config.Global.FixedSamplingPercentage;
             builder.UseSampling(fixedSamplingPercentage);
+            builder.Use(next => new HealthProbeTelemetryProcessor(next));
             builder.Build();
         }
     }

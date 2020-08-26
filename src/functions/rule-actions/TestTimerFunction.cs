1fe97886310e72a5f32739d98ab527497232b799
AARM6ZZ <aarm6zz@mmm.com>
2020-08-26 11:34:27 +0530

    Corrected errors and text

--- src/functions/rule-actions/TestTimerFunction.cs
+++ src/functions/rule-actions/TestTimerFunction.cs
@@ -1,3 +1,7 @@
+// <copyright file="TestTimerFunction.cs" company="3M">
+// Copyright (c) 3M. All rights reserved.
+// </copyright>
+
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;

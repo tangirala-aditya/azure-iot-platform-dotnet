33608ba9f106a9e9ac3a1d0ec35ec5accdaa1382
AARM6ZZ <aarm6zz@mmm.com>
2020-08-26 11:19:23 +0530

    Included test functions for verification

---
+++ src/functions/rule-actions/TestTimerFunction.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Mmm.Iot.Functions.RuleActions
+{
+    public static class TestTimerFunction
+    {
+        [FunctionName("TestTimerFunction")]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}

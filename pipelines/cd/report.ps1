abf67079dd7666f419f63f7d6068f5ee7f591b9e
Anant Choudhari <aarqmzz@mmm.com>
2020-09-17 21:07:46 +0530

    updated the variables to print

--- pipelines/cd/report.ps1
+++ pipelines/cd/report.ps1
@@ -284,13 +284,16 @@ Function Get-SerenityService {
                 if ($Null -ne $serenityService.PipelineRunId) {
                     Write-Verbose 'Getting Azure Pipeline run info'
                     Write-Verbose "serenityService.PipelineRunId : $serenityService.PipelineRunId"
+                    Write-Verbose $serenityService.PipelineRunId
                     $pipelineRun = Get-AzurePipelineRun -Id $serenityService.PipelineRunId
                     Write-Verbose "pipelineRunID :  $pipelineRun"
+                    Write-Verbose $pipelineRun
                     if ($pipelineRun) {
                         $serenityService.PipelineRunUrl = $pipelineRun.url
                         Push-Location (Join-Path $mmmSourceDirectory $serenityRepositoryName)
                         Write-Verbose 'Inspecting Git repository'
                         Write-Verbose "pipelineRun.sourceVersion : $($pipelineRun.sourceVersion)^{commit}"
+                        Write-Verbose $($pipelineRun.sourceVersion)^{commit}
                         $Null = git cat-file -e "$($pipelineRun.sourceVersion)^{commit}" 2>&1
                         if ($?) {
                             $serenityService.CommitDate = git show -s --format=%ci $pipelineRun.sourceVersion

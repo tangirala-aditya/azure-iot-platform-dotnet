1bd0f225b9e0ad06d48901ba0ffc6512d1d7b483
jnallagatla <66026384+jnallagatla@users.noreply.github.com>
2021-05-05 15:31:30 +0530

    update

--- src/webui/src/services/authService.js
+++ src/webui/src/services/authService.js
@@ -36,6 +36,10 @@ export class AuthService {
             loadUserInfo: false,
         };
         AuthService._userManager = new UserManager(AuthService.settings);
+        AuthService._userManager.events.addAccessTokenExpiring(function(){
+            console.log("token expiring...");
+            // AuthService._userManager.signinRedirect();
+        });
     }
 
     static isInvitation(hash) {

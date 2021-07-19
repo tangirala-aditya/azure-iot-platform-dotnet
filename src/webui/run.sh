d564f9e671ffe596baef1019b2ffe342e1e53dad
Sahith Gangineni <abfl5zz@mmm.com>
2021-07-19 15:30:13 +0530

    made changes to the docker file

--- src/webui/run.sh
+++ src/webui/run.sh
@@ -1,13 +1,13 @@
-/app/set_policies.sh
-/app/build.sh
+# /app/set_policies.sh
+# /app/build.sh
 
-if /app/set_env.sh; then # Set App config values for Auth Config
-  echo "Set Variables"
-else
-  exit 1
-fi
+# if /app/set_env.sh; then # Set App config values for Auth Config
+#   echo "Set Variables"
+# else
+#   exit 1
+# fi
 
 #Move new file to destination
-cp /app/webui-config.js /app/build/webui-config.js
+# cp /app/webui-config.js /app/build/webui-config.js
 echo "Starting server"
 nginx -c /app/nginx.conf
---
+++ src/webui/set_variables.sh
@@ -0,0 +1,5 @@
+if /app/set_env.sh; then # Set App config values for Auth Config
+  echo "Set Variables"
+else
+  exit 1
+fi

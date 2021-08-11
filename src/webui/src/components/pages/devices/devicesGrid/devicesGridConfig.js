30833b840826f9bbd3863f177a517ce79046857c
Viswanadh Kotha <Viswanadh.kotha@ggktech.com>
2021-08-11 11:37:46 +0530

    fixed build error

--- src/webui/src/components/pages/devices/devicesGrid/devicesGridConfig.js
+++ src/webui/src/components/pages/devices/devicesGrid/devicesGridConfig.js
@@ -69,8 +69,7 @@ export const deviceGridColumns = [
         cellRendererFramework: TimeRenderer,
         suppressSizeToFit: true,
         sort: "desc",
-    },
-};
+    }
 ];
 
 export const defaultDeviceColumns = [

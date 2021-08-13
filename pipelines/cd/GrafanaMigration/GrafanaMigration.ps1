9f66d8e24b2673958de21f5e1a3c47ce7bad9f90
tangirala-aditya <aditya.tangirala@acsicorp.com>
2021-08-13 17:00:17 +0530

    Added servicePrincipal details rather than env variable

--- pipelines/cd/GrafanaMigration/GrafanaMigration.ps1
+++ pipelines/cd/GrafanaMigration/GrafanaMigration.ps1
@@ -3,7 +3,10 @@
      [string] $environmentCategory,
      [string] $resourceGroup, 
      [string] $tenantId,
-     [string] $location
+     [string] $location,
+     [string] $servicePrincipalId,
+     [string] $servicePrincipalKey
+
 )
 function New-GrafanaApiKey {
      param(
@@ -112,16 +115,19 @@ function New-DataSources {
           [string] $environmentCategory,
           [string] $tenantId,
           [string] $grafanabaseurl,
-          [string] $grafanaApiKey
+          [string] $grafanaApiKey,
+          [string] $servicePrincipalId,
+          [string] $servicePrincipalKey
+
      )
 
      # creation of Data Sources for Grafana Dashboards
 
      $dataSourceContent = Get-Content '.\pipelines\cd\GrafanaMigration\sample-dataexplorer-datasource-template.json' -raw 
 
-     $dataSourceContent = $dataSourceContent -replace '\{0\}' , $env:servicePrincipalId
+     $dataSourceContent = $dataSourceContent -replace '\{0\}' , $servicePrincipalId
      $dataSourceContent = $dataSourceContent -replace '\{1\}' , $tenantId
-     $dataSourceContent = $dataSourceContent -replace '\{2\}' , $env:servicePrincipalKey
+     $dataSourceContent = $dataSourceContent -replace '\{2\}' , $servicePrincipalKey
      $dataSourceContent = $dataSourceContent -replace '\{3\}' , ("https://" + $applicationCode + "kusto" + $environmentCategory + ".centralus.kusto.windows.net")
 
 
@@ -144,9 +150,9 @@ function New-DataSources {
 
      $monitorDataSource = Get-Content '.\pipelines\cd\GrafanaMigration\sample-azuremonitor-datasource-template.json' -raw 
 
-     $monitorDataSource = $monitorDataSource -replace '\{0\}' , $env:servicePrincipalId
+     $monitorDataSource = $monitorDataSource -replace '\{0\}' , $servicePrincipalId
      $monitorDataSource = $monitorDataSource -replace '\{1\}' , $tenantId
-     $monitorDataSource = $monitorDataSource -replace '\{2\}' , $env:servicePrincipalKey
+     $monitorDataSource = $monitorDataSource -replace '\{2\}' , $servicePrincipalKey
 
 
      $monitorDataSourceBody = $monitorDataSource | ConvertFrom-Json | ConvertTo-Json -Depth 32
@@ -188,13 +194,13 @@ try {
 
      # Create DataSources required for Grafana Dashboards
      Write-Host "## Creating Data Sources"
-     New-DataSources -applicationCode $applicationCode -environmentCategory $environmentCategory -servicePrincipalId $env:servicePrincipalId -servicePrincipalKey $env:servicePrincipalKey -tenantId $tenantId -grafanabaseurl $grafanabaseurl -grafanaApiKey $grafanaApiKey
+     New-DataSources -applicationCode $applicationCode -environmentCategory $environmentCategory -servicePrincipalId $servicePrincipalId -servicePrincipalKey $servicePrincipalKey -tenantId $tenantId -grafanabaseurl $grafanabaseurl -grafanaApiKey $grafanaApiKey
      Write-Host "## Data Sources are created"
 
      Foreach ($iotHub in $iotHubArray) {
           $iotTenantId = $iotHub.TenantId
           # Create Main and Admin Dashboards for each tenant.
-          New-Dashboards -applicationCode $applicationCode -environmentCategory $environmentCategory -resourceGroup $resourceGroup -servicePrincipalId $env:servicePrincipalId -servicePrincipalKey $env:servicePrincipalKey -subscriptionId $subscriptionId -tenantId $tenantId -grafanabaseurl $grafanabaseurl -apptenantId $iotHub.TenantId -grafanaApiKey $grafanaApiKey
+          New-Dashboards -applicationCode $applicationCode -environmentCategory $environmentCategory -resourceGroup $resourceGroup -servicePrincipalId $servicePrincipalId -servicePrincipalKey $servicePrincipalKey -subscriptionId $subscriptionId -tenantId $tenantId -grafanabaseurl $grafanabaseurl -apptenantId $iotHub.TenantId -grafanaApiKey $grafanaApiKey
           Write-Host "Created Dashboard for Tenant:" + $iotTenantId        
      }
 }

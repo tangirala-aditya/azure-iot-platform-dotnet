5d2ba14b56f5edd07d7b912fd2c23ad15b00e1a1
tangirala-aditya <aditya.tangirala@acsicorp.com>
2021-09-30 15:02:12 +0530

    Added try catch expressions in populatemessage routing migration scripts

--- pipelines/cd/ExistingTenantsADE/PopulateMessageRouting.ps1
+++ pipelines/cd/ExistingTenantsADE/PopulateMessageRouting.ps1
@@ -11,6 +11,7 @@ param(
 )
       
      #remove and reinstall pkmngr and install packages
+  try {
 
      $cloudTable = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Context
      $tableObject = (Get-AzStorageTable -Name "tenant" -Context $cloudTable).CloudTable
@@ -30,31 +31,51 @@ param(
 
      Foreach ($iotHub in $iotHubArray) {
 
-          Write-Host "############## Started Updating routes in IotHub $iotHubName"
-
           $iotHubName = $iotHub.IotHubName
           $eventhubConnectionString=(Get-AzEventHubKey -ResourceGroupName $resourceGroupName -NamespaceName $eventhubNamespace -EventHub $eventHubs -AuthorizationRuleName "iothubroutes").PrimaryConnectionString
-     
-        # This is used to Create/Add an endpoint to all IoT Hub (for now it is single iothub)    
+
+          Write-Host "############## Started Updating routes in IotHub $iotHubName"
+          
+          $isRoutingEndPointExists=(Get-AzIotHubRoutingEndpoint -ResourceGroupName $resourceGroupName -Name $iotHubName | Where-Object {$_.Name -eq $endpointName}).Count
+
+          if($isRoutingEndPointExists -ge 0) {
+          # This is used to Create/Add an endpoint to all IoT Hub   
           Add-AzIotHubRoutingEndpoint `
-            -ResourceGroupName $resourceGroupName `
-            -Name $iotHubName `
-            -EndpointName $endpointName `
-            -EndpointType $endpointType `
-            -EndpointResourceGroup $resourceGroupName `
-            -EndpointSubscriptionId $subscriptionId `
-            -ConnectionString $eventhubConnectionString
+          -ResourceGroupName $resourceGroupName `
+          -Name $iotHubName `
+          -EndpointName $endpointName `
+          -EndpointType $endpointType `
+          -EndpointResourceGroup $resourceGroupName `
+          -EndpointSubscriptionId $subscriptionId `
+          -ConnectionString $eventhubConnectionString
 
           Write-Host "############## Added Routing Endpoint IotHub $iotHubName"
-
+          }
+          else {
+            Write-Host "############## Routing Endpoint Already Exists for Iothub $iotHubName "
+          }
+  
         for ($i = 0; $i -lt $messageRoutes.Count; $i++) {
-          # This is used to create a route in all IoT Hubs (for now it is single iothub)
-          az iot hub route create --hub-name $iotHubName --endpoint-name $endpointName --source $dataSources[$i] --enabled true --condition true -n $messageRoutes[$i] --resource-group $resourceGroupName
-         } 
 
-         Write-Host "############## Added Routes IotHub $iotHubName"
+          $isMessageRouteExists=$(az iot hub route list -g $resourceGroupName --hub-name $iotHubName --query "[?name=='$dataSources[$i]'] | length(@)")
+          
+          if($isMessageRouteExists -ne 0) {
+            # This is used to create a route in all IoT Hubs
+            az iot hub route create --hub-name $iotHubName --endpoint-name $endpointName --source $dataSources[$i] --enabled true --condition true -n $messageRoutes[$i] --resource-group $resourceGroupName
+            Write-Host "############## Added Routes IotHub $iotHubName"           
+          }
+          else {
+            Write-Host "############## Route $messageRoutes[$i] Already Exists with Source $dataSources[$i] for IotHub $iotHubName"            
+          }          
+         
+         } 
 
          az iot hub message-enrichment update --key "tenant" --value $iotHub.TenantId  --endpoints "event-hub-telemetry" "event-hub-twin-change" "event-hub-lifecycle" "event-hub-device-twin-mirror" -n $iotHubName         
 
          Write-Host "############## Added Message Enrichment IotHub $iotHubName"
     }
+  }
+  catch {
+      Write-Host("An Error occured.")
+      Write-Host($_)
+  }    
